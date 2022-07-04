685426badee38946a7e607d02ac73e60f267cfc6
vinicius <vinicius.ribeiro@solvimm.com>
2022-07-04 17:28:00 -0300

    Refactor echo setup

--- backend/main.go
+++ backend/main.go
@@ -5,8 +5,31 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+func newService() *Service {
+	service := &Service{
+		Server: echo.New(),
+	}
+
+	return service
+}
+
 func main() {
-	e := echo.New()
+	
+	service := newService()
+
+	//Middleware
+	service.Server.Use(
+		middleware.CORSWithConfig(middleware.CORSConfig{
+			AllowMethods: []string{echo.GET, echo.POST},
+			AllowOrigins: []string{"*"},
+		}),
+		middleware.Logger(),
+		middleware.Recover(),
+		middleware.RequestID(),	
+	)
+	
+
+
 
 	// v1.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
 	// 	// Be careful to use constant time comparison to prevent timing attacks
@@ -22,14 +45,10 @@ func main() {
 
 	// e.Use(middleware.Logger())
 	// e.Use(middleware.Recover())
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowMethods: []string{echo.GET, echo.POST},
-		AllowOrigins: []string{"*"},
-	}))
 
-	e.Static("/", "/app")
+	// e.Static("/", "/app")
 
 	// e.GET("/login", routes.LoginPage)
 
-	e.Logger.Fatal(e.Start(":8080"))
+	service.Server.Logger.Fatal(service.Server.Start(":8080"))
 }
